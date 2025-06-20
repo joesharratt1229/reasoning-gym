210bdbc054f2b132b5962e80f3f1486af846b317
Zafir Stojanovski <zaf.stojano@gmail.com>
2025-06-21 00:01:31 +0200

    tutorial(training): Add a minimal example with `trl` (#473)
    * v0
    
    * 2 gpu setup
    
    * improve parsing from yaml
    
    * update yaml dataset example
    
    * remove restriction on flash attn
    
    * more comments
    
    * first version of the readme
    
    * pin torch
    
    * simplify requirements
    
    * just flash attn
    
    * use set env instead
    
    * simpler set env
    
    * readme
    
    * add wandb project to setup
    
    * update template
    
    * update model id
    
    * post init to capture the config and weight
    
    * extract metadata
    
    * update config
    
    * update dataset config
    
    * move env for wandb project
    
    * pre-commit
    
    * remove qwen-math from training
    
    * more instructions
    
    * unused import
    
    * remove trl old
    
    * warmup ratio
    
    * warmup ratio
    
    * change model id
    
    * change model_id
    
    * add info about CUDA_VISIBLE_DEVICES

--- examples/trl/set_env.sh
+++ examples/trl/set_env.sh
@@ -2,19 +2,11 @@
 # python 3.10 + cuda 11.8.0
 # the execution order the following commands matter
 
-export MKL_NUM_THREADS=1
-export NUMEXPR_NUM_THREADS=1
-export OPENBLAS_NUM_THREADS=1
-export OMP_NUM_THREADS=1
-
 conda clean -a -y
-mamba clean -a -y
 pip install --upgrade pip
 pip cache purge
 
-# cuda, gcc/g++, torch
-# mamba install cuda -c nvidia/label/cuda-11.8.0 -y
-# mamba install gcc gxx -c conda-forge -y
+# torch
 pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu118
 
 # xformers
@@ -25,15 +17,7 @@ pip install https://github.com/vllm-project/vllm/releases/download/v0.7.2/vllm-0
 
 pip install deepspeed
 pip install flash-attn==2.7.3 --no-build-isolation
-pip install peft
-
 pip install "trl==0.15.2"
-pip install latex2sympy2_extended
-pip install "math_verify==0.5.2"
-pip install word2number
-pip install scipy
-
+pip install "transformers==4.49.0"
 pip install wandb
-pip install plotly
-pip install matplotlib
-pip install seaborn
+pip install reasoning-gym
